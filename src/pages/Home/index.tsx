2da307b59affca5e4d1e841de83b79c344ed930e
fernandosantoshs <fernandosantoshs@gmail.com>
2022-11-12 19:14:09 -0300

    Added typeScript, tsconfig and started Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import './style.css'
+
+export function Home(){
+    return(
+        <div className="container">
+            <h1>Ola</h1>
+        </div>
+    )
+}
