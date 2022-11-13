c32e6ff47b533cf8c8bbb08b987a51e181f136cd
fernandosantoshs <fernandosantoshs@gmail.com>
2022-11-13 11:24:59 -0300

    Added global.css, google font & css reset

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,9 +2,27 @@ import React, { useState, useEffect } from "react";
 import './style.css'
 
 export function Home(){
+    const [user, setUser] = useState({} as User)
+
+
+    type User = {
+        name:string,
+        avatar: string
+    }
+
+    
+
     return(
         <div className="container">
-            <h1>Ola</h1>
+            <h1>Lista de presença</h1>
+
+            <input type="text"
+            placeholder="Digite o nome.."
+            />
+
+            <button>Adicionar</button>
+
+            {/* <Card></Card> */}
         </div>
     )
 }
