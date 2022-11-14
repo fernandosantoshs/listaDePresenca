0624dff444eb34d46b35e8c811cfbd430e1d99ff
fernandosantoshs <fernandosantoshs@gmail.com>
2022-11-14 10:56:58 -0300

    Feats:CSS and useEffect function for user info

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,48 @@
 import React, { useState, useEffect } from "react";
-import './style.css'
+import "./style.css";
 
-export function Home(){
-    const [user, setUser] = useState({} as User)
+export function Home() {
+  const [user, setUser] = useState({} as User);
 
+  type User = {
+    name: string;
+    avatar: string;
+  };
 
-    type User = {
-        name:string,
-        avatar: string
-    }
+//   type UserResponse = {
+//     name: string,
+//     avatar: string
+//   }
 
-    
+  useEffect(() => {
+    fetch('http://api.github.com/users/fernandosantoshs')
+    .then(response => response.json())
+    .then(data => { 
+        setUser({
+        name: data.name,
+        avatar: data.avatar_url
+    })
+    })
+}
 
-    return(
-        <div className="container">
-            <h1>Lista de presença</h1>
+  )
 
-            <input type="text"
-            placeholder="Digite o nome.."
-            />
-
-            <button>Adicionar</button>
-
-            {/* <Card></Card> */}
+  return (
+    <div className="container">
+      <header>
+        <h1>Lista de presença</h1>
+        <div>
+            <strong>{user.name}</strong>            
+            <img src={user.avatar} alt="Foto de perfil" />
         </div>
-    )
+      </header>
+
+        <input type="text" placeholder="Digite o nome.." />
+
+        <button>Adicionar</button>
+      
+
+      {/* <Card></Card> */}
+    </div>
+  );
 }
